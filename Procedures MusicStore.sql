0f2d584ce9df1913267f51c37e65cf2e2608c1e6
OleksiiShevchukRepository <oleksii.shevchuk.n@gmail.com>
2016-03-24 14:06:42 +0200

    Update Procedures MusicStore.sql

--- Procedures MusicStore.sql
+++ Procedures MusicStore.sql
@@ -16,7 +16,7 @@ GO
 
 --Shows all albums of selected genre in music shop storage, no filters
 CREATE PROCEDURE spAlbumsInMusicStore_Filters
-	@GenreId INT
+	@GenreId INT --review O.S: it's necessary to validate input arguments
 AS
 BEGIN
 	SELECT alb.Id as id, alb.Name as albName, art.Name as artName, g.Name as genre, ass.PriceRealisation as price,
@@ -54,12 +54,12 @@ GO
 CREATE PROCEDURE spCreateSoldItem
 	@AlbumId INT,
 	@Amount INT,
-	@Price DECIMAL(19, 2)
+	@Price DECIMAL(19, 2)--review O.S: it's necessary to validate input arguments
 AS
 BEGIN
 	DECLARE @CheckID INT;
 	DECLARE	@CheckDate DATETIME;
-	SET @CheckID = (SELECT TOP 1 Id FROM tblCheck ORDER BY Id DESC);
+	SET @CheckID = (SELECT TOP 1 Id FROM tblCheck ORDER BY Id DESC);--review O.S: it's better to use Scope_Identity()
 	SET @CheckDate = (SELECT TOP 1 DateStatement FROM tblCheck ORDER BY DateStatement DESC)
 	INSERT INTO tblSoldItem (AlbumId, CheckId, SellDate, Amount, SumItems)
 	VALUES (@AlbumId, @CheckID, @CheckDate, @Amount, @Price);
